8d11e956b4d0faf5d775ab466bc695d6472b966c
AtharvaPC <baodhankaratharva@gmail.com>
2024-07-19 08:57:47 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Inspired by: 
+// https://codepen.io/sgorneau/pen/mWEddP
+// as described in the above codepen, if we set the value `x` to something like -100% it'll go too fast
+// so I took the function to calculate the rate and applied to my code
