b4cfd94ff0b334e53667febb48c9f9e9ea80b3ac
AtharvaPC <baodhankaratharva@gmail.com>
2024-07-20 10:17:04 +0530

    Added navbar with responsiveness

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-// Inspired by: 
-// https://codepen.io/sgorneau/pen/mWEddP
-// as described in the above codepen, if we set the value `x` to something like -100% it'll go too fast
-// so I took the function to calculate the rate and applied to my code
+const hamburger = document.querySelector(".hamburger");
+const navBar = document.querySelector("#navbar");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("is-active");
+    navBar.classList.toggle("is-active");
+});
+
