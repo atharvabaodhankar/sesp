ab7ccb3168af2d89050c9105ccb4307abea765d7
AtharvaPC <baodhankaratharva@gmail.com>
2024-07-23 20:09:50 +0530

    added quicklinks

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
+const quickHamburger = document.querySelector(".quick-links-button");
 const navBar = document.querySelector("#navbar");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("is-active");
     navBar.classList.toggle("is-active");
+});
+
+quickHamburger.addEventListener("click", () => {
+    quickHamburger.parentElement.classList.toggle("is-active");
 });
 
 // Quick Links
